c1e6e618100ac30f0f9a2229eb68bf3f1b66f06d
KonradUdoHannes <konrad.woelms@gmail.com>
2019-05-25 15:50:48 +0200

    Changed views to use new unique item forms

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -8,7 +8,10 @@ from unittest import skip
 
 from lists.views import home_page
 from lists.models import Item,List
-from lists.forms import ItemForm,EMPTY_ITEM_ERROR
+from lists.forms import (
+    ItemForm,EMPTY_ITEM_ERROR,
+    DUPLICATE_ITEM_ERROR,ExistingListItemForm
+)
 
 class HomePageTest(TestCase):
 
@@ -55,7 +58,7 @@ class ListViewTest(TestCase):
     def test_display_item_form(self):
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
-        self.assertIsInstance(response.context['form'],ItemForm)
+        self.assertIsInstance(response.context['form'],ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
 class NewListTest(TestCase):
@@ -82,7 +85,7 @@ class NewListTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.client.post('/lists/new',data={'text':''})
-        self.assertIsInstance(response.context['form'],ItemForm)
+        self.assertIsInstance(response.context['form'],ExistingListItemForm)
 
     def post_invalid_input(self):
         list_ = List.objects.create()
@@ -138,7 +141,6 @@ class NewListTest(TestCase):
 
         self.assertRedirects(response,f'/lists/{correct_list.id}/')
     
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_list_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -146,7 +148,7 @@ class NewListTest(TestCase):
         f'/lists/{list1.id}/',
         data={'text': 'textey'}
         )
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
